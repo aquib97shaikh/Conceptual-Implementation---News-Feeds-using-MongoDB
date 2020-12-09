f939345144fad0ff30057d6d0ffd8e9686ffcbea
Aquib <aquibshaikh02397@gmail.com>
2020-12-09 17:33:30 +0530

    added code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const { newsArticleModel } = require('./connector')
 
 const onePageArticleCount = 10
 
@@ -9,7 +10,13 @@ const onePageArticleCount = 10
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+app.get("/newFeeds",async (req,res)=>{
+    let limit = Number(req.query.limit);
+    let offset = Number(req.query.offset);
+    offset = Number.isNaN(offset) ? 0:offset;
+    limit = Number.isNaN(limit) ? onePageArticleCount :limit;
+    res.send(await newsArticleModel.find({}).skip(offset).limit(limit));
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
